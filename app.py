df49cda38a8ab8a4fbbe586ef525e58d815ab992
Dwarg <arturbakowski@pm.me>
2023-10-12 20:48:05 +0200

    Added rate limiting and negotiating response content.

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, request
+from flask_limiter import Limiter
+import yaml
 import os
 
 app = Flask(__name__)
- 
-@app.route("/")
-def index():
-    return "<p>Hello, World!</p>"
- 
-if __name__ == '__main__':  
+limiter = Limiter(app, default_limits=["5 per minute"], headers_enabled=True)
+ip_addresses = []
+
+@app.route('/', methods=['GET'])
+@limiter.limit("2 per minute") # Set a rate limit of 2 requests per minute
+def get_ip_address():
+    ip = request.remote_addr
+    ip_addresses.append(ip)
+
+    accept_header = request.headers.get('Accept', 'text/plain')
+    if 'text/html' in accept_header:
+        return f"<h1>Your IP address is {ip}</h1>"
+    elif 'application/xml' in accept_header:
+        return f"<ip><address>{ip}</address></ip>",  {'Content-Type': 'application/xml'}
+    elif 'application/yaml' in accept_header:
+        return yaml.dump({'ip': {'address': ip}}), {'Content-Type': 'application/yaml'}
+    else:
+        return f"Your IP address is {ip}", {'Content-Type': 'text/plain'}
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
